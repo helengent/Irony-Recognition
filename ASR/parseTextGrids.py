1dae2574e9dc5a8cbebbd752cf8333c17f3e2cd6
Helen <hmgent2@illinois.edu>
2021-04-26 10:30:58 -0500

    started something. Might quit

---
+++ ASR/parseTextGrids.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+from glob import glob
+
+
+def lineUp(words, phones):
+
+    combined = dict()
+
+    for w in words:
+
+        wordPhones = [p for p in phones if p[1] >= w[1] and p[2] <= w[2]]
+        combined[w] = wordPhones
+
+    return combined
+
+
+def parse(f):
+    with open(f, "r") as t:
+        raw = t.read()
+    lines = raw.split("\n")
+
+    fileStart = float(lines[3])
+    fileEnd = float(lines[4])
+
+    phones = list()
+    words = list()
+
+    assert lines[8] == '"phone"'
+
+    #lines[9] should be fileStart
+    #lines[10] should be fileEnd
+    #lines[11], I believe, is the number of phones in the utterance
+
+    i = 12
+
+    while i < len(lines) and lines[i] != '"IntervalTier"':
+        phoneStart = float(lines[i])
+        phoneEnd = float(lines[i+1])
+        phone = lines[i+2][1:-1]
+
+        phones.append((phone, phoneStart, phoneEnd))
+
+        i += 3
+
+    i += 1
+
+    assert lines[i] == '"word"'
+    i += 4
+
+    while i < len(lines) - 1:
+        wordStart = float(lines[i])
+        wordEnd = float(lines[i+1])
+        word = lines[i+2][1:-1]
+
+        words.append((word, wordStart, wordEnd))
+
+        i += 3
+
+    return fileStart, fileEnd, words, phones
+
+
+def main(input_dir):
+    
+    fileList = glob("{}/*.TextGrid".format(input_dir))
+
+    print(len(fileList))
+
+    for f in fileList:
+        start, end, words, phones = parse(f)
+        lineUp(words, phones)
+
+
+if __name__=="__main__":
+
+    input_dir = "data/ANH_manual"
+
+    main(input_dir)
