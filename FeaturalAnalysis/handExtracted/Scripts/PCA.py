c79c1382494fef1a483449a46635395947fcd485
Helen Gent <hmgent2@iona.linguistics.illinois.edu>
2021-10-13 10:13:16 -0500

    update

--- FeaturalAnalysis/handExtracted/Scripts/PCA.py
+++ FeaturalAnalysis/handExtracted/Scripts/PCA.py
@@ -11,11 +11,138 @@ from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
 
 
+def plotPCs3D(finalDF):
+
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+
+    ax.set_xlabel('Principal Component 1', fontsize = 10)
+    ax.set_ylabel('Principal Component 2', fontsize = 10)
+    ax.set_zlabel('Principal Component 3', fontsize = 10)
+    ax.set_title('3 component PCA', fontsize = 15)
+
+    targets = ['I', 'N']
+    colors = ['g', 'b']
+    for target, color in zip(targets,colors):
+        indicesToKeep = finalDF['label'] == target
+        ax.scatter3D(finalDF.loc[indicesToKeep, 'PC1']
+                , finalDF.loc[indicesToKeep, 'PC2']
+                , finalDF.loc[indicesToKeep, 'PC3']
+                , alpha = 0.5
+                , c = color
+                , s = 50)
+    ax.legend(targets)
+    #ax.grid()
+    plt.savefig("../Output/3_factorPCA.png")
+
+    plt.clf()
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+
+    ax.set_xlabel('Principal Component 1', fontsize = 10)
+    ax.set_ylabel('Principal Component 2', fontsize = 10)
+    ax.set_zlabel('Principal Component 3', fontsize = 10)
+    ax.set_title('3 component PCA - Ironic', fontsize = 15)
+
+    indicesToKeep = finalDF['label'] == 'I'
+    ax.scatter3D(finalDF.loc[indicesToKeep, 'PC1']
+            , finalDF.loc[indicesToKeep, 'PC2']
+            , finalDF.loc[indicesToKeep, 'PC3']
+            , alpha = 0.5
+            , c = 'g'
+            , s = 50)
+    ax.legend(targets)
+    plt.savefig("../Output/3_factorPCA_Ironic.png")
+
+    plt.clf()
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+
+    ax.set_xlabel('Principal Component 1', fontsize = 10)
+    ax.set_ylabel('Principal Component 2', fontsize = 10)
+    ax.set_zlabel('Principal Component 3', fontsize = 10)
+    ax.set_title('3 component PCA - Non-Ironic', fontsize = 15)
+
+    indicesToKeep = finalDF['label'] == 'N'
+    ax.scatter3D(finalDF.loc[indicesToKeep, 'PC1']
+            , finalDF.loc[indicesToKeep, 'PC2']
+            , finalDF.loc[indicesToKeep, 'PC3']
+            , alpha = 0.5
+            , c = 'b'
+            , s = 50)
+    ax.legend(targets)
+    plt.savefig("../Output/3_factorPCA_Non-Ironic.png")
+
+
+def plotPCs(finalDF):
+
+    #Plot the first two PCs in different colors for ironic and non-ironic samples
+    fig = plt.figure(figsize = (8,8))
+    ax = fig.add_subplot(1,1,1) 
+    ax.set_xlabel('Principal Component 1', fontsize = 15)
+    ax.set_ylabel('Principal Component 2', fontsize = 15)
+    ax.set_title('2 component PCA', fontsize = 20)
+    
+    targets = ['I', 'N']
+    colors = ['g', 'b']
+    for target, color in zip(targets,colors):
+        indicesToKeep = finalDF['label'] == target
+        ax.scatter(finalDF.loc[indicesToKeep, 'PC1']
+                , finalDF.loc[indicesToKeep, 'PC2']
+                , c = color
+                , s = 50)
+    ax.legend(targets)
+    ax.grid()
+    plt.savefig("../Output/2_factorPCA.png")
+
+    plt.clf()
+    fig = plt.figure(figsize = (8,8))
+    ax = fig.add_subplot(1,1,1) 
+    ax.set_xlabel('Principal Component 1', fontsize = 15)
+    ax.set_ylabel('Principal Component 2', fontsize = 15)
+    ax.set_title('2 component PCA - Ironic', fontsize = 20)
+    
+    indicesToKeep = finalDF['label'] == 'I'
+    ax.scatter(finalDF.loc[indicesToKeep, 'PC1']
+            , finalDF.loc[indicesToKeep, 'PC2']
+            , c = 'g'
+            , s = 50)
+    ax.legend(targets)
+    ax.grid()
+    plt.savefig("../Output/2_factorPCA_Ironic.png")
+
+    plt.clf()
+    fig = plt.figure(figsize = (8,8))
+    ax = fig.add_subplot(1,1,1) 
+    ax.set_xlabel('Principal Component 1', fontsize = 15)
+    ax.set_ylabel('Principal Component 2', fontsize = 15)
+    ax.set_title('2 component PCA - Non-Ironic', fontsize = 20)
+    
+    indicesToKeep = finalDF['label'] == 'N'
+    ax.scatter(finalDF.loc[indicesToKeep, 'PC1']
+            , finalDF.loc[indicesToKeep, 'PC2']
+            , c = 'b'
+            , s = 50)
+    ax.legend(targets)
+    ax.grid()
+    plt.savefig("../Output/2_factorPCA_Non-Ironic.png")
+
+
+def plotVariance(v):
+
+    #Plot the percentage of variance explained with the addition of PCs
+    plt.ylabel("% Variance Explained")
+    plt.xlabel("# of Features")
+    plt.title("PCA Analysis")
+    plt.ylim(0, 100)
+    plt.style.context("seaborn-whitegrid")
+
+    plt.plot(v)
+    plt.savefig("../Output/PCA_analysis.png")
+
+
 def main(df, numComps):
 
-    # features = ["f0globalMean", "f0globalRange", "f0globalSD", "f0globalMedian",
-                # "energyRange", "energySD", "hnrglobalMean", "hnrglobalRange", "hnrglobalSD", 
-                # "duration", "sound2silenceRatio", "totalPauses", "Avg. Word Dur.", "Avg. Sil. Dur.", "SyllPerSecond"]
     features = df.columns[3:].tolist()
 
     x = df.loc[:, features].values
@@ -39,41 +166,18 @@ def main(df, numComps):
     variance = pca.explained_variance_ratio_
     var = np.cumsum(np.round(variance, decimals=3)*100)
     print(var)
-
-    #Plot the percentage of variance explained with the addition of PCs
-    plt.ylabel("% Variance Explained")
-    plt.xlabel("# of Features")
-    plt.title("PCA Analysis")
-    plt.ylim(0, 100)
-    plt.style.context("seaborn-whitegrid")
-
-    plt.plot(var)
-    plt.savefig("../Output/PCA_analysis.png")
+    plotVariance(var)
+    plotPCs(finalDF)
+    plotPCs3D(finalDF)
 
 
-    #Plot the first two PCs in different colors for ironic and non-ironic samples
-    fig = plt.figure(figsize = (8,8))
-    ax = fig.add_subplot(1,1,1) 
-    ax.set_xlabel('Principal Component 1', fontsize = 15)
-    ax.set_ylabel('Principal Component 2', fontsize = 15)
-    ax.set_title('2 component PCA', fontsize = 20)
-    
-    targets = ['I', 'N']
-    colors = ['g', 'b']
-    for target, color in zip(targets,colors):
-        indicesToKeep = finalDF['label'] == target
-        ax.scatter(finalDF.loc[indicesToKeep, 'PC1']
-                , finalDF.loc[indicesToKeep, 'PC2']
-                , c = color
-                , s = 50)
-    ax.legend(targets)
-    ax.grid()
 
 
 if __name__=="__main__":
 
     # df = pd.read_csv("~/Data/AcousticData/text_feats/Pruned3_asr_text_feats.csv")
     df = pd.read_csv("../Data/all_narrowed.csv")
+    df.drop(columns=["ZCR"])
     print(df)
     print(df.shape)
     numComps = [6]
