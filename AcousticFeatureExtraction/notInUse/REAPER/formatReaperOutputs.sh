5da09482d370e2d4b2fdee20e0b91b462bb15beb
Helen Gent <hmgent2@iona.linguistics.illinois.edu>
2022-02-04 11:33:21 -0600

    cleaning up

--- AcousticFeatureExtraction/notInUse/REAPER/formatReaperOutputs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#This script is meant to be executed in the directory where the .f0 files are
-
-for i in *.f0
-do
-    echo $i
-    awk -F" " '{ if($2==1) {print $3;} else {print "NaN";}}' $i >> $i.p
-done
